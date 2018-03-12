75969632ed84ac2624f3a3ebd16c77d3069def50
spoeck <toni.spoeck@web.de>
2018-03-12 18:12:29 +0100

    add hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReduxRoot from './ReduxRoot';
 
-ReactDOM.render(<ReduxRoot />, document.querySelector('#root'));
+const rootEl = document.getElementById('root');
+ReactDOM.render(<ReduxRoot />, rootEl);
+
+if (module.hot) {
+    module.hot.accept('./ReduxRoot', () => {
+        const NextApp = require('./ReduxRoot').default;
+        ReactDOM.render(
+            <NextApp />,
+            rootEl
+        );
+    });
+}
