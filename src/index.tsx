b0f872d5983bf8ff74b509634782d2ce76587892
PatStadler <p.stadler@innfactory.de>
2020-02-04 17:20:07 +0100

    service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { ReduxRoot } from "./ReduxRoot";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { ReduxRoot } from './ReduxRoot';
+import register from './registerServiceWorker';
 
 const rootEl = document.getElementById("root");
 ReactDOM.render(<ReduxRoot />, rootEl);
 
 // comment in for PWA with service worker in production mode
-// registerServiceWorker();
+register();
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -10,16 +10,16 @@
 
 const isLocalhost = Boolean(
 	window.location.hostname === "localhost" ||
-		// [::1] is the IPv6 localhost address.
-		window.location.hostname === "[::1]" ||
-		// 127.0.0.1/8 is considered localhost for IPv4.
-		window.location.hostname.match(
-			/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-		)
+	// [::1] is the IPv6 localhost address.
+	window.location.hostname === "[::1]" ||
+	// 127.0.0.1/8 is considered localhost for IPv4.
+	window.location.hostname.match(
+		/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+	)
 );
 
 export default function register() {
-	if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+	if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator || isLocalhost) {
 		// The URL constructor is available in all browsers that support SW.
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
 		if (publicUrl.origin !== window.location.origin) {
@@ -46,7 +46,7 @@ export default function register() {
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
 						"This web app is being served cache-first by a service " +
-							"worker. To learn more, visit https://goo.gl/SC7cgQ"
+						"worker. To learn more, visit https://goo.gl/SC7cgQ"
 					);
 				});
 			} else {
@@ -96,7 +96,7 @@ function checkValidServiceWorker(swUrl) {
 			if (
 				response.status === 404 ||
 				response.headers.get("content-type").indexOf("javascript") ===
-					-1
+				-1
 			) {
 				// No service worker found. Probably a different app. Reload the page.
 				navigator.serviceWorker.ready.then(registration => {
